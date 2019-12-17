3b3c57ff7fc07e5c90e6452a611fc93b2c356af0
ismail <ic.ismailcengiz@gmail.com>
2019-12-18 00:57:43 +0300

    first commit

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import {Text,View} from 'react-native'
+import React from 'react'
+
+export default function Home ({xyz}){
+return(
+
+        <Text>
+{xyz}
+            hello home page
+        </Text>
+)
+   
+}
